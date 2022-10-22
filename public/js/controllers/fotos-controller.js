fe630129d53ae26b52e7f5b7e5d184b1c8967dd0
alexandre-dev <alexandre-dev@DESKTOP-1677OJS.local>
2022-10-22 20:42:51 -0300

    primeiro commit

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController', function($scope){
+    $scope.foto = {
+        titulo: 'Leao',
+        url: "https://st.depositphotos.com/1325441/1966/i/950/depositphotos_19661835-stock-photo-lion-king-of-the-wild.jpg"
+    }
+});
